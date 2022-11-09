263c1cb0c607835bd67493734f10c3dd0f76d674
Oleksandr Gannuschchenko <ipz204_gov@student.ztu.edu.ua>
2022-11-09 14:54:10 +0200

    changed first 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Практична робота №2 Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Практична робота №2 Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Практична робота №2 Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
